36bcddb5d282c99cda75dbc112d7b18ee289c4c3
Kyrylo Kalashnikov <kir486680@gmail.com>
2022-08-27 15:43:32 -0400

    Live User Diagnostic

--- diagnostic.py
+++ diagnostic.py
@@ -13,30 +13,23 @@ def dist2deg(desiredDistance, diameter):
 
 gripper = hub.port.D.motor
 arm = hub.port.B.motor #the highest it goes is  -120
-x_wheels = hub.port.E.motor
+x_wheels = hub.port.C.motor
 # - - -> forward 
 # + - -> forward 
 # - + <- back 
 # + + -< back
-y_wheels = hub.port.A.motor
+y_wheels = hub.port.F.motor
 # -, - -> 
 # +, - -> 
 #+, + <-
 #-, + <-
-x_wheels.run_for_degrees(-270, -30) 
-time.sleep(2)
-x_wheels.run_for_degrees(270, -30)
-time.sleep(2)
-x_wheels.run_for_degrees(-270, 30)
-time.sleep(2)
-x_wheels.run_for_degrees(270, 30)
-
-"""
-print(gripper.get())
-gripper.run_for_degrees(-20,-30)
-print(gripper.get())
-time.sleep(1)
-gripper.run_for_degrees(20,30)
-print(gripper.get())
-"""
-
+while True:
+    userInput = input("Please enter your command:")
+    if userInput.lower() == "done":
+        print("Shutting Down")
+        break
+    try:
+        exec(userInput)
+    except Exception as e:
+        print(e)
+        print("Try again!")
