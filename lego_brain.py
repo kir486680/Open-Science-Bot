9d891f0402b170523f3f4deadd35f3bc2468168b
Kyrylo Kalashnikov <kir486680@gmail.com>
2023-05-26 15:25:50 -0400

    Changed Gantry Format

--- lego_brain.py
+++ lego_brain.py
@@ -4,6 +4,7 @@ import time
 
 
 class Gantry:
+
     def __init__(self, gear_ratios=[1, 1, 1], motor_resolution=360):
         self.gear_ratios = np.array(gear_ratios)  # gear ratios for rack and pinion in X, Y, Z
         self.motor_resolution = motor_resolution  # degrees of rotation for one full rotation of the motor
@@ -19,33 +20,82 @@ class Gantry:
         
 
     def set_cm_per_rotation(self, cm_per_rotation):
-        # set cm moved per full rotation of the gear in X, Y, Z
+        """
+        Set the cm per rotation for the robot
+        
+        Args:
+            cm_per_rotation (list): list of cm per rotation for each axis
+        
+        Returns:
+            None
+        """
         self.cm_per_rotation = np.array(cm_per_rotation)
 
     def motor_to_cm(self, motor_rotations):
-        # convert motor rotation to cm
+        """
+        Convert motor rotations to cm
+
+        Args:
+            motor_rotations (array): array of motor rotations for each axis
+
+        Returns:
+            distances_moved (array): array of distances moved for each axis
+        """
         gear_rotations = motor_rotations / self.gear_ratios
         distances_moved = gear_rotations * self.cm_per_rotation
         return distances_moved
 
     def cm_to_motor(self, distances_cm):
-        # convert cm to motor rotation
+        """
+        Convert cm to motor rotations
+
+        Args:
+            distances_cm (array): array of distances moved for each axis
+
+        Returns:
+            motor_rotations (array): array of motor rotations for each axis
+        """
         gear_rotations = distances_cm / self.cm_per_rotation
         motor_rotations = gear_rotations * self.gear_ratios
         return motor_rotations
 
     def forward_kinematics(self, motor_rotations):
-        # calculate forward kinematics
+        """
+        Calculate forward kinematics
+
+        Args:
+            motor_rotations (array): array of motor rotations for each axis
+
+        Returns:
+            current_position (array): array of current position for each axis
+        """
         self.current_position = self.motor_to_cm(motor_rotations)
         return self.current_position
 
     def inverse_kinematics(self, target_position):
-        # calculate inverse kinematics
+        """
+        Calculate inverse kinematics
+
+        Args:
+            target_position (array): array of target position for each axis
+
+        Returns:
+            motor_rotations (array): array of motor rotations for each axis
+        """
         motor_rotations = self.cm_to_motor(target_position - self.current_position)
         self.current_position = target_position
         return motor_rotations
 
     def move_to_position(self, target_position):
+        '''
+        Move the robot to the target position
+        
+        Args:
+            target_position (array): array of target position for each axis
+
+        Returns:
+            None
+        '''
         if self.hub != None:
             # move motors to target position
             motor_rotations = self.inverse_kinematics(target_position)
