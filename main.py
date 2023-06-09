956954ffa4abe8a4f33fc086e44d318010e8ae72
jaegpark <jaepark.g@gmail.com>
2023-06-09 16:40:53 -0400

    remove multithreading

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 import numpy as np 
 from lego_brain import Gantry
-import concurrent.futures
 from pump import Pump
 from camera import Camera
 from gripper import Gripper
 import threading
 import cv2
-from tasks import GripperTask, PumpTask, CameraTask, MoveRobotTask
 #init the main 
 
 
 def main():
     gantry = Gantry()
     pump = Pump([23,24,25])
-    camera = Camera()
+    #pump.pump_liquid(15)
+    #camera = Camera()
     gantry.set_cm_per_rotation([4, 12.56, 5])
     gripper = Gripper(17)
     
+    target_position = np.array([0, 0,-4])
+    gantry.move_to_position(target_position)
+    gripper.ungrip()
 
-    gripper_task = GripperTask(gripper)
-    #pump_task = PumpTask(pump)
-    camera_task = CameraTask(camera)
-    move_robot_task = MoveRobotTask(gantry)
+    
+    def pump_task():
+        
+        print("Pumping")
+    
+    def camera_task():
+        image = camera.get_image() 
+        print(image.shape)
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        executor.submit(gripper_task.execute())
-        #executor.submit(pump_task.execute())
-        executor.submit(camera_task.execute())
-        executor.submit(move_robot_task.execute())
-
-        concurrent.futures.wait([gripper_task, camera_task, move_robot_task])
+ 
+    #image = camera.get_image() 
+    # cv2.imshow("image", image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    print("Hello World")
 
 
 if __name__ == "__main__":
