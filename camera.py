c114ff7ade83ec8b5ed184c80e1d909003227362
thejammerr <jaepark.g@gmail.com>
2023-06-13 12:59:39 -0400

    add init for mock classes

--- camera.py
+++ camera.py
@@ -1,4 +1,21 @@
-import picamera
+try:
+    import picamera
+except (ImportError, ValueError):
+    # We're not on a Raspberry Pi - use a mock camera
+    class MockCamera:
+        def __init__(self):
+            print("Initialising camera mock")
+        
+        def start_preview(self):
+            print("Starting preview mock")
+
+        def stop_preview(self):
+            print("Stopping preview mock")
+
+        # Add more methods as needed based on what you use from picamera.
+    
+    picamera = MockCamera
+
 import time
 import cv2
 import numpy as np
