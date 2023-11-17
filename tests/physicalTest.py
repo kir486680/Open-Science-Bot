6a5635aa3469712abf0098ea67b66ab7221d561f
jaegpark <jaepark.g@gmail.com>
2023-11-17 15:35:10 -0500

    Made it look like a clean package

--- tests/physicalTest.py
+++ tests/physicalTest.py
@@ -1,19 +1,18 @@
 import numpy as np
 import cv2
-from lego_brain import Gantry
-from pump import Pump
-from camera import Camera
-from gripper import Gripper
+from autolab.lego_brain import Gantry
+from autolab.pump import Pump
+from autolab.camera import Camera
+from autolab.gripper import Gripper
 import time
 
 # init the main
 
 
-def main():
+def test_right_gripper():
     gantry = Gantry()
     #Initializing the pump with the defaul GPIO pins
-    #pump = Pump([23,24,25])
-    #pump.pump_liquid(15)
+    
     #camera = Camera()
     gantry.set_cm_per_rotation([4, 12.56, 5,5])
     #gripper = Gripper(17)
@@ -22,14 +21,12 @@ def main():
     #first parameter in array is x, second is y, third is z of the gripper with counter electrode, fourth is z of gripper with working electrode
     target_position = np.array([0, 0, 0, 0])
     gantry.move_to_position(target_position)
-    # target_position = np.array([1.5, 0, 1.5, 0])
-    # gantry.move_to_position(target_position)
-    # target_position = np.array([1.5, 0, 0, 0])
-    # gantry.move_to_position(target_position)
-    # target_position = np.array([0, 0, 3.5, 3.5])
-    # gantry.move_to_position(target_position)
-    # target_position = np.array([0, 0, 0, 0])
-    # gantry.move_to_position(target_position)
+    target_position = np.array([1.5, 0, 2.5, 0])
+    gantry.move_to_position(target_position)
+    target_position = np.array([0, 0, 3.5, 0])
+    gantry.move_to_position(target_position)
+    target_position = np.array([0, 0, 0, 0])
+    gantry.move_to_position(target_position)
     #gripper.ungrip()
     # image = camera.get_image()
     # cv2.imshow("image", image)
@@ -39,4 +36,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    for i in range(10):
+        test_right_gripper()
